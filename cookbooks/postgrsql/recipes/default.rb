b985de162763ce4f2f01688c8fbc9dab5228268f
abe-1 <avijitbhowmik2003@yahoo.com>
2018-04-20 20:06:21 +0000

    adding postgrsql cookbook

---
+++ cookbooks/postgrsql/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook:: postgrsql
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'postgrsql-server' do
+	notifies :run, 'execute[postgrsql-init]'
+end
+
+execure 'postgrsql-init' do
+	command 'postgrsql-setup initdb'
+	action :nothing
+end
+
+service 'postgrsql' do
+	action [:enable, :start]
+end
